5da3ee03392a7577ef5991af26a2a6f3611468a1
tom-konda <tom.konda.dev@gmail.com>
2018-03-12 13:06:19 +0900

    Rewrite CLI code in async/await

--- src/ts/bin/cli.ts
+++ src/ts/bin/cli.ts
@@ -1,95 +1,53 @@
 import commander = require('commander');
 import fs = require('fs');
+import { promisify } from 'util';
 const packageInfo = JSON.parse(fs.readFileSync(`./package.json`).toString());
 const sc2tojson = <SC2toJSONStatic>require('../index');
 
 const fileAccessCheck = (inputFile: string) => {
-  return new Promise(
-    (resolve, reject) => {
-      let isfileReadable: number;
-      if (typeof fs.constants === 'undefined') {
-        // for Node 4.x
-        isfileReadable = fs.R_OK;
-      }
-      else {
-        isfileReadable = fs.constants.R_OK;
-      }
-      fs.access(
-        inputFile,
-        isfileReadable,
-        (error) => {
-          error ? reject(error) : resolve(inputFile);
-        }
-      );
-    }
+  return promisify(fs.access)(
+    inputFile,
+    fs.constants.R_OK
   );
 }
 
-const fileFormatCheck = (inputfile: string) => {
-  return new Promise(
-    (resolve, reject) => {
-      const file = fs.readFileSync(inputfile);
-      const uint8arr = new Uint8Array(file);
-      try {
-        const sc2Data = sc2tojson.outputJSONText(uint8arr.buffer);
-        resolve(sc2Data);
-      }
-      catch (error) {
-        reject(error);
-      }
-    }
-  );
+const fileFormatCheck = (inputFile: Buffer) => {
+  const uint8arr = new Uint8Array(inputFile);
+  try {
+    const sc2Data = sc2tojson.outputJSONText(uint8arr.buffer as ArrayBuffer);
+    return Promise.resolve(sc2Data);
+  }
+  catch (error) {
+    return Promise.reject(error);
+  }
 }
 
-const outputJSON = (JSONtext:string, options: any) => {
-  return new Promise(
-    (resolve, reject) => {
-      const outputFile = options.output;
-      if (outputFile) {
-        fs.writeFile(
-          outputFile,
-          JSONtext,
-          (error) => {
-            error ? reject(error) : resolve(outputFile);
-          }
-        )
-      }
-      else {
-        process.stdout.write(JSONtext);
-        resolve();
-      }
+const convertSC2toJSON = async (inputSC2File: string, options: any) => {
+  try {
+    await fileAccessCheck(inputSC2File);
+    const file = await promisify(fs.readFile)(inputSC2File);
+    const JSONText = await fileFormatCheck(file);
+    if (options.output) {
+      await promisify(fs.writeFile)(options.output, JSONText);
+      process.stdout.write(`${options.output} was created successfully.\n`);
     }
-  )
-}
-
-const convertSC2toJSON = (inputSC2File: string, options: any) => {
-  fileAccessCheck(inputSC2File)
-    .then(
-    fileFormatCheck,
-    (error) => {
-      console.error('Cannot Read File.');
+    else {
+      process.stdout.write(JSONText);
+    }
+  }
+  catch (error) {
+    console.log(error);
+    console.error('Cannot write a JSON file.');
+    if(error instanceof RangeError) {
+      process.stderr.write(`-1 : ${error.message}\n`);
+      process.exit(-1);
+    }
+    else if (error as NodeJS.ErrnoException instanceof Error) {
       process.stderr.write(`${error.errno} : ${error.message}\n`);
       process.exit(error.errno);
     }
-    ).then(
-    (json:string) => outputJSON(json, options),
-    (error: SyntaxError) => {
-      console.error('This is wrong SC2K file.');
-      console.error(error);
-      process.stderr.write(`${error.name} : ${error.message}\n`);
-      process.exit(1);
-    }
-    ).then(
-    (outputFile) => {
-      outputFile ? process.stdout.write(`${outputFile} was created successfully.\n`) : null;
-    },
-    (error) => {
-      console.error('Cannot write a JSON file.');
-      process.stderr.write(`${error.errno} : ${error.message}\n`);
-      process.exit(error.errno);
-    }
-    );
-}
+  }
+};
 
 commander.version(packageInfo.version)
   .command('<inputFile>', 'Path to a SC2K .sc2 or .scn file')
