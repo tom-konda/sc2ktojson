e376e0a1cd5929cb7287fde8e64ffea45b44b73d
Tom Konda <tom.konda.dev@gmail.com>
2023-04-20 11:09:24 +0900

    Remove unused files

--- rollup-configs/rollup.config.cjs.js
+++
@@ -1,8 +0,0 @@
-export default {
-  input: './lib/sc2ktojson.js',
-  output: [
-    { file: 'lib/sc2ktojson.cjs.js', format: 'cjs'},
-    { file: 'index.js', format: 'cjs' },
-  ],
-  external: ['fs'],
-}
