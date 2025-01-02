6192ed286ab9d6db64192dbfba7cf109dbd0cf8e
Tom Konda <tom.konda.dev@gmail.com>
2025-01-03 01:02:10 +0900

    Upgrade ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        indent: ["error", 2],
+        "no-var": "error",
+        "no-constant-condition": "error",
+        "no-trailing-spaces": "error",
+        "no-multiple-empty-lines": "error",
+        "space-before-blocks": "error",
+        "arrow-spacing": "error",
+        "space-before-function-paren": ["error", "never"],
+        "space-in-parens": ["error", "never"],
+        "space-infix-ops": "error",
+        "space-unary-ops": "error",
+        "@typescript-eslint/explicit-function-return-type": "off",
+    },
+}];
